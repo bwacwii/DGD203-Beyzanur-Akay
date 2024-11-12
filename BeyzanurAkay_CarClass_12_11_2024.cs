d069b2b7636b6bd96aef6e07e10e4118e0e4b64b
Beyzanur Akay <163772414+bwacwii@users.noreply.github.com>
2024-11-12 14:42:28 +0300

    Add files via upload

---
+++ BeyzanurAkay_CarClass_12_11_2024.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace CarGame;
+
+public class Car
+{
+    public float Fuel { get; private set; }
+    public float Torque { get; private set; }
+
+    public int WheelCount;
+
+    public string FuelType;
+
+    public string CarType;
+
+    public float Speed;
+
+    public Car(float fuel)
+    {
+        Fuel = Math.Clamp(fuel, 0f, 100f);
+        if (fuel < 0) fuel = 0;
+        Fuel = fuel;
+
+        Torque = 200f;
+        WheelCount = 4;
+        FuelType = "Diesel";
+        CarType = "manual" ;
+        Speed = 0f;
+    }
+
+  
+}
+
+    
+
+
+
+
+
+
